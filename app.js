5ef7af65ac4fb38c052e577e272413a30f5b3be1
shnoozlemcdoozle <esheena1@gmail.com>
2017-07-26 20:25:57 -0400

    testing

--- app.js
+++ app.js
@@ -164,7 +164,7 @@ io.sockets.on('connection', function (socket, username) {
     })
 
     socket.on('missionVoteBtnOnClick', function () {
-        io.to(eitan).emit('beginMissionVote');
+        io.to('eitan').emit('beginMissionVote');
         /*
         socket.emit('beginMissionVote');
         socket.broadcast.emit('beginMissionVote'); */
